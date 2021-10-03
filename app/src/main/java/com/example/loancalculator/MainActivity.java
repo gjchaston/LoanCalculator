6677c1f0dbfc42b3bfb578ba2d91cde504e0f84e
Gareth Chaston <gjchaston@gmail.com>
2021-10-03 15:55:55 -0400

    Finished output and minimum payment, project complete

--- app/src/main/java/com/example/loancalculator/MainActivity.java
+++ app/src/main/java/com/example/loancalculator/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText loanAmount, interestRate, loanTerm, loanFees;
+    private EditText loanAmount, interestRate, loanTerm, loanFees, minPayment;
     private Button calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         interestRate = (EditText) findViewById(R.id.interestRate);
         loanTerm = (EditText) findViewById(R.id.loanTerm);
         loanFees = (EditText) findViewById(R.id.loanFees);
+        minPayment = (EditText) findViewById(R.id.minPayment);
         calculate = (Button) findViewById(R.id.calculateButton);
 
         calculate.setOnClickListener(new View.OnClickListener() {
@@ -30,13 +31,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 double principle = Double.parseDouble(loanAmount.getText().toString());
                 double interest = Double.parseDouble(interestRate.getText().toString()) / 100;
-                double terms = Double.parseDouble(loanTerm.getText().toString());
+                double terms = Double.parseDouble(loanTerm.getText().toString())*12;
                 double fees = Double.parseDouble(loanFees.getText().toString());
-                int total = (int) (principle * (Math.pow((1 + (interest/12)), terms * 12)) + fees);
+                double min = 0.00;
+                min = Double.parseDouble(minPayment.getText().toString());
+                double paymnt = 0;
+
+                int total = (int) (principle * (Math.pow((1 + (interest/12)), terms)) + fees);
+                paymnt = Double.valueOf(total/terms);
+                if((total/terms)<min){
+                    terms = total/min;
+                    paymnt = min;
+                }
 
                 Toast.makeText(getApplicationContext(),
-                        "$" + String.valueOf(total) + ".00",
+                        "Total: $" + String.valueOf(total) + ".00\n" +"Payment: $" + String.valueOf(paymnt) +"/month for "+ (int)terms + " months",
                         Toast.LENGTH_LONG).show();
+
             }
         });
     }
