fd9baa57df76f9b9bd6d37d9bb635529e0a9c4ab
FardinIqbal <52869603+FardinIqbal@users.noreply.github.com>
2021-10-02 23:04:59 -0400

    Finished project but the formula for the total might be off.

--- app/src/main/java/com/example/loancalculator/MainActivity.java
+++ app/src/main/java/com/example/loancalculator/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.loancalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText loanamount, interestrate, loanterm, loanfees;
+    private EditText loanAmount, interestRate, loanTerm, loanFees;
     private Button calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +19,25 @@ public class MainActivity extends AppCompatActivity {
         addListenerOnButton();
     }
     public void addListenerOnButton() {
-        loanamount = (EditText) findViewById(R.id.loanAmount);
-        interestrate = (EditText) findViewById(R.id.interestRate);
-        loanterm = (EditText) findViewById(R.id.loanTerm);
-        loanfees = (EditText) findViewById(R.id.loanFees);
-//        calculate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String value1 = loanamount.getText().toString();
-//                String value2 = interestrate.getText().toString();
-//                String value3 = loanterm.getText().toString();
-//                String value4 = null;
-//                if (loanfees.getText().toString() != null) {
-//
-//                }
-//            }
-//        });
+        loanAmount = (EditText) findViewById(R.id.loanAmount);
+        interestRate = (EditText) findViewById(R.id.interestRate);
+        loanTerm = (EditText) findViewById(R.id.loanTerm);
+        loanFees = (EditText) findViewById(R.id.loanFees);
+        calculate = (Button) findViewById(R.id.calculateButton);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double principle = Double.parseDouble(loanAmount.getText().toString());
+                double interest = Double.parseDouble(interestRate.getText().toString()) / 100;
+                double terms = Double.parseDouble(loanTerm.getText().toString());
+                double fees = Double.parseDouble(loanFees.getText().toString());
+                int total = (int) (principle * (Math.pow((1 + (interest/12)), terms * 12)) + fees);
+
+                Toast.makeText(getApplicationContext(),
+                        "$" + String.valueOf(total) + ".00",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
